110be3ba06cb6bddade0f970b4343f7a4aabfc7c
Danilo Rocha <contato_dsr@hotmail.com>
2022-10-01 16:13:45 -0300

    att

--- Exercícios 61 a 70/Ex61.java
+++ Exercícios 61 a 70/Ex61.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 class Ex61 {
     public static void main (String[] args){
-    /*Implemente um programa em Java que a partir de dois números
-informados pelo usuário, calcule o dobro de cada número e apresente
-ao usuário. */
-        Scanner inputScan = new Scanner (System.in);
+    try (/*Implemente um programa em Java que a partir de dois números
+        informados pelo usuário, calcule o dobro de cada número e apresente
+        ao usuário. */
+        Scanner inputScan = new Scanner (System.in)) {
+            String num1;
+
+            System.out.print("Digite dois números: ");
+            num1 = inputScan.nextLine();
+
+            System.out.println(dobroFun(num1));
+        }
 
         // Exercício 61 - Dobro de 2 números.
 
-        String num1;
-
-        System.out.print("Digite dois números: ");
-        num1 = inputScan.nextLine();
-
-        System.out.println(dobroFun(num1));
 
 
 
