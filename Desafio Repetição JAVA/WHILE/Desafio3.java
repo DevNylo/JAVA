da978235f40443a092bef51479a756f8f1916aea
Danilo Rocha <contato_dsr@hotmail.com>
2022-10-16 11:48:33 -0300

    Create Desafio3.java

---
+++ Desafio Repetição JAVA/WHILE/Desafio3.java
@@ -0,0 +1,14 @@
+class Desafio3 {
+  public static void main(String[] args) {
+
+    int valor = 100000;
+    int i = 0;
+
+    while (i < valor) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+      i++;
+    }
+  }
+}
