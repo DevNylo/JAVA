da94aa08da72d2af90d0b57c7aa2e0238ed3ae4f
Danilo Rocha <contato_dsr@hotmail.com>
2022-10-16 11:44:07 -0300

    Create Ex83.java

---
+++ Exercicios 76 a 85 ADO 8/Ex83.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Ex83 {
+  public static void main(String[] args) {
+    Scanner inputScan = new Scanner(System.in);
+
+    // Exercício 83 - Orçamento familiar
+
+    System.out.println("Digite o ganho da familia");
+    double ganho = inputScan.nextDouble();
+
+    System.out.println("Digite o gasto da familia");
+    double gasto = inputScan.nextDouble();
+
+    String resultado = calcFun(ganho, gasto);
+    System.out.println(resultado);
+
+  }
+
+  public static String calcFun(double ganho, double gast) {
+    double valor = (gast * 100) / ganho;
+
+    String result = "";
+
+    if (valor <= 30) {
+      result = "Parabéns, está gerenciando bem seu orçamento! ";
+    } else if (valor <= 50) {
+      result = "Muito bem, seus gastos não ultrapassam metade dos ganhos! ";
+    } else if (valor <= 80) {
+      result = "Atenção, melhor conter os gastos!";
+    } else if (valor <= 100) {
+      result = "Cuidado, seu orçamento pode ficar comprometido!";
+    } else {
+      result = "Orçamento comprometido! Hora de rever seus gastos!";
+    }
+    return result;
+  }
+}
