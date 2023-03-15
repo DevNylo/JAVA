55337e616926ab8dd83ba9754a0d491792a1fab1
Danilo Rocha <contato_dsr@hotmail.com>
2023-03-15 18:55:20 -0300

    ALgumas alt

--- POO/Heranca/heranca/src/App.java
+++ POO/Heranca/heranca/src/App.java
@@ -1,8 +1,5 @@
-package application;
-
-public class Program {
+public class App {
     public static void main(String[] args) throws Exception {
-        
-        
+        System.out.println("Hello, World!");
     }
 }
